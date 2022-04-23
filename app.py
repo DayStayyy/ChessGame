ffad7267122aae7783f08758d3353159d6f2da16
Adrien CLAMADIEU-THARAUD <adrien.clamadieu@ynov.com>
2022-04-23 13:46:12 +0200

    api board

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from chess import Chess
 
 app = Flask(__name__)
 app.run(debug=True)
 
+game = Chess('player1','player2')
 
 @app.route('/')
-def hello():
-    game = Chess('player1','player2')
+def home():
     board = game.getBoardJson()
     jsonFile = open("data/board.json", "w")
     jsonFile.write(board)
     jsonFile.close()
 
     return render_template('index.html') 
+
+@app.route('/api/board', methods=['GET'])
+def board():
+    jsonFile = open("data/board.json", "r")
+    board = jsonFile.read()
+    jsonFile.close()
+
+    return jsonify(board) 
