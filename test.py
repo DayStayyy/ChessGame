72d4576b9bb7139c900485ca5c666bd1b4111f33
DayStayyy <daystayyy@gmail.com>
2022-05-23 16:41:43 +0200

    test

--- test.py
+++ test.py
@@ -1,10 +1,51 @@
-from flask import Flask
+import unittest
+from Mychess import Chess
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+    
+class MychessFunc():
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    def __init__(self) -> None:
+        self.createMyChess()
+    
+    def createMyChess(self):
+        self.chessGame =  Chess('player1','player2')
+        return self.chessGame
+
+
+    def createChessFromJson(self):
+        self.jsonGame = self.chessGame.newGame()
+        return  self.jsonGame
+
+    def testJsonToFen(self,turn):
+        return self.chessGame.jsonToFen(self.jsonGame,turn)
+
+    
+
+    def testJsonToGame(self):
+        chessFunc.createChessFromJson()
+        return chessFunc.testJsonToGame()
+    
+
+chessFunc = MychessFunc()
+
+class testMychess(unittest.TestCase):
+
+    def testCreateMyChess(self) :
+        self.assertIsNotNone(chessFunc.createMyChess())
+    
+    def testCreateChessFromJson(self):
+        self.assertIsNotNone(chessFunc.createChessFromJson())
+    
+    def testJsonToFen(self):
+        chessFunc.createChessFromJson()
+        self.assertEqual(chessFunc.testJsonToFen(1), "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1")
+        self.assertEqual(chessFunc.testJsonToFen(2), "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+
+    def jsonToGame(self):
+        self.assertIsNotNone(chessFunc.testJsonToGame())
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
